536b7ad20cd3c71c579ae591a547c08250e2a33c
andrewmamdouh122 <134093499+andrewmamdouh122@users.noreply.github.com>
2025-05-31 00:49:52 +0300

    Update upgarde.sh

--- upgarde.sh
+++ upgarde.sh
@@ -1,11 +1,9 @@
 python3 -c 'import pty; pty.spawn("/bin/bash")'
 
-
 export TERM=xterm-256color
 export SHELL=/bin/bash
 export HOME=/home/$(whoami)
 export USER=$(whoami)
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-
 
 bash --login -i
